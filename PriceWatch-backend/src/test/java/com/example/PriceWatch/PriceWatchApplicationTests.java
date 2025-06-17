6632aaa0a9df849e17e39aef1e127bb2860c76e3
Fabian3050 <137364972+Fabian3050@users.noreply.github.com>
2025-06-16 23:02:12 -0400

    backend added

---
+++ PriceWatch-backend/src/test/java/com/example/PriceWatch/PriceWatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PriceWatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceWatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
