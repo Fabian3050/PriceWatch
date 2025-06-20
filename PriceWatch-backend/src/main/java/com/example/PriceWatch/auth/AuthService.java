e809c583c360e1f259f44d65bb0d873fd9f590aa
Fabian3050 <137364972+Fabian3050@users.noreply.github.com>
2025-06-20 14:28:34 -0400

    Jwt was added

--- PriceWatch-backend/src/main/java/com/example/PriceWatch/auth/AuthService.java
+++ PriceWatch-backend/src/main/java/com/example/PriceWatch/auth/AuthService.java
@@ -1,4 +1,47 @@
 package com.example.PriceWatch.auth;
 
+import com.example.PriceWatch.entities.RoleEntity;
+import com.example.PriceWatch.entities.UserEntity;
+import com.example.PriceWatch.jwt.JwtService;
+import com.example.PriceWatch.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class AuthService {
+    private final UserRepository userRepository;
+    private final JwtService jwtService;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthResponse login(LoginRequest request) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
+        UserDetails user = userRepository.findByUsername(request.getUsername()).orElseThrow();
+        String token = jwtService.getToken(user);
+        return AuthResponse.builder()
+                .token(token)
+                .build();
+    }
+
+    public AuthResponse register(RegisterRequest request) {
+        UserEntity user = UserEntity.builder()
+                .username(request.getUsername())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .country(request.getCountry())
+                .role(RoleEntity.USER)
+                .build();
+
+        userRepository.save(user);
+
+        return AuthResponse.builder()
+                .token(jwtService.getToken(user))
+                .build();
+    }
 }
