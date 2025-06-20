a5bf52b840c4e73d60bb28d03910995d0bd1de24
Fabian3050 <137364972+Fabian3050@users.noreply.github.com>
2025-06-20 15:47:46 -0400

    conection db and fix to jwt

--- PriceWatch-backend/src/main/java/com/example/PriceWatch/config/SecurityConfig.java
+++ PriceWatch-backend/src/main/java/com/example/PriceWatch/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.PriceWatch.config;
 
+import com.example.PriceWatch.jwt.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
